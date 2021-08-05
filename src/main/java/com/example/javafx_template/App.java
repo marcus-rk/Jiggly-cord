9c6f33fcd2722ce299e5e597927a7cd8b1c2d474
marcus-rk <88247953+marcus-rk@users.noreply.github.com>
2021-08-05 23:55:08 +0200

    Add files via upload
    First mark-up of login page done.

---
+++ src/main/java/com/example/javafx_template/App.java
@@ -0,0 +1,43 @@
+package com.example.javafx_template;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * This is a template to make JavaFX projects (with or without CSS).
+ * Remember to make use of MVC and SceneBuilder.
+ * Open SceneBuilder by right-clicking on the App.xml file in the 'resources' folder
+ */
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.setTitle("Jiggly-Cord v1");
+
+        // Add icon to application window
+        Image iconImage = new Image(getClass().getResource("LoginPage_Image/Jiggly_icon.png").toExternalForm());
+        stage.getIcons().add(iconImage);
+
+        // Link css stylesheet to scene
+        String css = this.getClass().getResource("LoginPage.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        // Should always be at the end of the start-method
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.javafx_course_3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.javafx_template to javafx.fxml;
+    exports com.example.javafx_template;
+}
