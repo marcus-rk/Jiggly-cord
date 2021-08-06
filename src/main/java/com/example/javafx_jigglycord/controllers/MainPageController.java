85b61e9d2392cf8544aa518d95d9d339b32edac6
marcus_desktop <marcus.rappenborg@gmail.com>
2021-08-06 22:09:01 +0200

    MainPage now have a template

--- src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
+++ src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
@@ -1,4 +1,9 @@
 package com.example.javafx_jigglycord.controllers;
 
+import javafx.event.ActionEvent;
+
 public class MainPageController {
+    public void sendMessage(ActionEvent actionEvent) {
+
+    }
 }
