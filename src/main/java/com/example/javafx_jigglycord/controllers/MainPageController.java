41163d879cb6e422214412c4b0252a66e6a21a7c
marcus_desktop <marcus.rappenborg@gmail.com>
2021-08-07 02:05:02 +0200

    Updated CSS for MainPage

--- src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
+++ src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
@@ -1,29 +1,11 @@
 package com.example.javafx_jigglycord.controllers;
 
-import com.example.javafx_jigglycord.App;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
-import javafx.geometry.VPos;
-import javafx.scene.DepthTest;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextFlow;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainPageController {
     @FXML
@@ -32,28 +14,28 @@ public class MainPageController {
     private TextField textField;
 
 
-    public void sendMessage(ActionEvent event) throws IOException {
-        addDialogBox();
+    public void sendMessage(){
+        addDialogBoxToVBOX();
         textField.clear();
     }
 
     @FXML
-    void addDialogBox() {
+    void addDialogBoxToVBOX() {
         // Updates dialogVBOX with new DialogBox
         dialogVBOX.getChildren().add(createDialogBox());
     }
 
     private Pane createDialogBox(){
         // Create new pane
-        Pane pane = new Pane();
+        StackPane pane = new StackPane();
         pane.setPrefSize(100.0,100.0);
-        pane.setStyle("-fx-background-color: #00797a;");
+        pane.setId("dialogPane");
 
         // Add text to pain
-        Text text = new Text();
-        text.setText(textField.getText());
+        Label text = new Label();
+        text.setText("  "+textField.getText());
+        text.setId("dialogPaneText");
         pane.getChildren().add(text);
-        System.out.println(pane.getChildren().toString());
 
         return pane;
     }
