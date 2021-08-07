9057931ea3b6eb3d924dd28bd785a225cca025ba
marcus_desktop <marcus.rappenborg@gmail.com>
2021-08-07 05:51:27 +0200

    made the easy way out and made Global var. Old func is line commented

---
+++ src/main/java/com/example/javafx_jigglycord/Global.java
@@ -0,0 +1,7 @@
+package com.example.javafx_jigglycord;
+
+import java.io.File;
+
+public class Global {
+    public static File currentUserFile;
+}
--- src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
+++ src/main/java/com/example/javafx_jigglycord/controllers/MainPageController.java
@@ -1,5 +1,6 @@
 package com.example.javafx_jigglycord.controllers;
 
+import com.example.javafx_jigglycord.Global;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -8,6 +9,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
 public class MainPageController {
     @FXML
     private VBox dialogVBOX;
@@ -15,27 +18,30 @@ public class MainPageController {
     private TextField textField;
 
 
-    public void sendMessage(){
+    public void sendMessage() throws IOException {
         addDialogBoxToVBOX();
         textField.clear();
     }
 
     @FXML
-    void addDialogBoxToVBOX() {
+    void addDialogBoxToVBOX() throws IOException {
         // Updates dialogVBOX with new DialogBox
         dialogVBOX.setAlignment(Pos.BOTTOM_CENTER); // From bottom -> top
         dialogVBOX.getChildren().add(createDialogBox());
     }
 
-    private Pane createDialogBox(){
+    private Pane createDialogBox() throws IOException {
+        UserController currentUser = UserController.getUserFromFile(Global.currentUserFile);
+
         // Create new pane
         StackPane pane = new StackPane();
-        pane.setPrefSize(100.0,100.0);
+        pane.setPrefSize(100.0,20.0);
         pane.setId("dialogPane");
 
         // Add text to pain
         Label text = new Label();
-        text.setText("  "+textField.getText());
+        String usernameString = "  "+currentUser.getUsername()+": ";
+        text.setText(usernameString+textField.getText());
         text.setId("dialogPaneText");
         pane.getChildren().add(text);
 
